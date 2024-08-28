4ab74c308616ca143f7d975f9c1f42bd66782afc
matcodesalot <reallymathew@gmail.com>
2024-08-28 18:00:48 -0500

    add basic crud and bootstrap

---
+++ README.md
@@ -0,0 +1,48 @@
+# YelpCamp
+
+## Description
+YelpCamp aims to inform the user about nationwide campgrounds. Similar to Yelp, you will be able to see reviews about the campground you are interested in.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+
+
+## Installation
+
+### Prerequisites
+Make sure you have the following installed on your machine:
+- [Node.js](https://nodejs.org/) (v14.x or higher)
+- [npm](https://www.npmjs.com/) (usually comes with Node.js)
+- [MongoDB](https://www.mongodb.com/) (v4.x or higher)
+- [Nodemon](https://www.npmjs.com/package/nodemon) (optional, but I like it. npm install -g nodemon)
+
+### Steps
+
+1. **Clone the repository:**
+```
+git clone https://github.com/matcodesalot/yelpcamp.git
+cd yelpcamp
+```
+
+2. **Install dependencies:**
+```
+npm install
+```
+
+3. **Start MongoDB: Make sure MongoDB is running on your machine. You can start it with:**
+```
+mongod
+```
+
+4. **Run the application:**
+```
+node app.js
+or
+nodemon app.js
+```
+
+## Usage
+
+After setting up and running the application, you can:
+- Access the web interface at http://localhost:3000.
---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected.");
+});
+
+const path = require('path');
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+//show all campgrounds
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", {campgrounds});
+});
+
+//render the form to input a new campground
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+//create a new campground (post)
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//show specific campground by id
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", {campground});
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", {campground});
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//delete campground by id
+app.delete("/campgrounds/:id", async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+});
+
+app.listen(3000, () => {
+    console.log("Serving on port 3000");
+});
