faf8339c2c85fa75056f97f63206d219ea665e25
matcodesalot <reallymathew@gmail.com>
2024-08-29 14:53:36 -0500

    add images and basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const randomPrice = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: "Lorem ipsum odor amet, consectetuer adipiscing elit. Cursus imperdiet sapien ullamcorper orci magnis sollicitudin interdum volutpat. Tristique justo curae phasellus quis eros ultrices.",
+            price: randomPrice
         })
         await camp.save();
     }
@@ -27,4 +31,4 @@ const seedDB = async () => {
 
 seedDB().then(() => {
     mongoose.connection.close();
-})
+});
